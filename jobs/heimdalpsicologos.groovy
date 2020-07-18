1f39649028449c2fb6ab3e9afb8ffec3d68dcc13
TeloggyDevYaherCarrillo <63134237+TeloggyDevYaherCarrillo@users.noreply.github.com>
2020-07-17 21:25:38 -0500

    Req - Conf : Conf Jenkins as a Code From Heimdal Psicologos

---
+++ jobs/heimdalpsicologos.groovy
@@ -0,0 +1,25 @@
+project_name = "heimdal-psicologos"
+repo = "https://github.com/Teloggy/Heimdal-Psicologos-Front.git"
+repo_name = "heimdal-psicologos"
+
+pipelineJob (project_name){
+    definition{
+	triggers{
+	    scm('0 */2 * * *')
+	}
+	cpsScm{
+	    scm{
+		git{    		    
+		    remote{
+			name(repo_name)
+			url(repo)
+			refspec(null)
+		        credentials('yaher-teloggy-easy')
+		    }
+		    branches('master')		    
+                }
+		scriptPath("Jenkinsfile")
+	    }
+	}
+    }
+}
